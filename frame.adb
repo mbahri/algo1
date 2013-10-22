406ae2f5067b5dd7e2e7a8c5dc4c65b3275545f9
lapostoj <jerome.lapostolet@ensimag.grenoble-inp.fr>
2013-10-22 14:57:54 +0200

    frame done

--- frame.adb
+++ frame.adb
@@ -8,8 +8,17 @@ with Scene;
 package body Frame is
 
     procedure Calcul_Image is
+        P1, P2, P3 : Vecteur(1..2);
+        Nb_Facette:Natural:=0;
     begin
         -- a faire : calcul des projections, affichage des triangles
+        Nb_Facette:=Nombre_De_Facettes;
+        for i in 1..Nb_Facette loop
+            Projection_Facette(i, P1, P2, P3);
+            Tracer_Segment(P1(1), P1(2), P2(1), P2(2));
+            Tracer_Segment(P1(1), P1(2), P2(1), P2(2));
+            Tracer_Segment(P2(1), P2(2), P3(1), P3(2));
+        end loop;
         null;
     end;
 
