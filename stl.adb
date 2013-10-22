73f977f1356ffc888d580551e4fcf9d6b278b7dc
Mehdi BAHRI <mehdi.bahri@ensimag.grenoble-inp.fr>
2013-10-22 13:49:26 +0200

    Modification de la fonction d'épuration

--- stl.adb
+++ stl.adb
@@ -2,33 +2,41 @@
 with Ada.Text_IO;
 use Ada.Text_IO;
 
+-- On utilise des chaines de longueur variable pour le traitement des lignes
+with Ada.Strings.Unbounded;
+
 package body STL is
+
+    package U renames Ada.Strings.Unbounded;
 
     function Nombre_Facettes(Nom_Fichier : String) return Natural is
         F : File_Type;
         Nb : Natural := 0;
+        Line_epuree : U.Unbounded_String := U.Null_Unbounded_String;
     begin
         Open(File => F, Mode => In_File, Name => Nom_Fichier);
         -- Fonctionnement du comptage : on compte les endfacet
         -- On lit le fichier ligne par ligne, pour chaque ligne on supprime tous les caractères qui ne sont pas
         -- des lettres minuscules.
-        -- Pour ce faire, on crée une copie de la ligne et on écrase les caractères en partant du début.
+        -- On recopie pour cela tous les caractères différents de ' ' dans une Unbounded_String.
+        -- ANCIENNE VERSION : Pour ce faire, on crée une copie de la ligne et on écrase les caractères en partant du début.
         -- On compare ensuite la partie de la chaine correspondant à la ligne épurée : ce sont les "cpt" premiers caractères.
 
         while not End_of_File(F) loop
             declare
                 Line : String := Get_Line(File);
-                Line_epuree : String := Line;
                 cpt : Natural := 1;
             begin
+                Line_epuree := U.Null_Unbounded_String;
+
                 for i in Line'Range loop
-                    if Line(i) in 'a'..'z' then
-                        Line_epuree(cpt) := Line(i);
-                        cpt := cpt + 1;
-                    end if;
+                     if Line(i) /= ' ' then
+                         U.Append(Source => Line_epuree, New_Item => Line(i));
+                         cpt := cpt + 1;
+                     end if;
                 end loop;
 
-                if Line_epuree(Line_epuree'First .. (cpt-1)) = "endfacet" then
+                if U.To_String(Line_epuree) = "endfacet" then
                     Nb := Nb + 1;
                 end if;
             end;
