0ecc5c20d07630fdba2399ef821f4fcfc59d029a
Mehdi BAHRI <mehdi.bahri@ensimag.grenoble-inp.fr>
2013-10-22 14:44:04 +0200

    T

--- stl.adb
+++ stl.adb
@@ -1,3 +1,5 @@
+with Algebre;
+use Algebre;
 
 with Ada.Text_IO;
 use Ada.Text_IO;
@@ -7,6 +9,7 @@ with Ada.Strings.Unbounded;
 
 package body STL is
 
+    -- On pourra utiliser le package Ada.Strings.Unbounded sous le nom U
     package U renames Ada.Strings.Unbounded;
 
     function Nombre_Facettes(Nom_Fichier : String) return Natural is
@@ -46,10 +49,66 @@ package body STL is
         return Nb;
     end;
 
+
+    function Parse_ligne(Ligne : in String) return Vecteur is
+        Facette : Vecteur(1..3);
+        Pos : Positive := 1;
+        CarCour : Character := Ligne(1);
+
+        Buffer : U.Unbounded_String := U.Null_Unbounded_String;
+
+        procedure AvCar is
+        begin
+            Pos := Pos + 1;
+            CarCour := Ligne(Pos);
+        end;
+
+        function Lire6 return String is
+        begin
+            Buffer := U.Null_Unbounded_String;
+            for I in 1..6 loop
+                U.Append(Source => Buffer, New_Item => CarCour);
+                AvCar;
+            end loop;
+
+            return U.To_String(Buffer);
+        end;
+
+        -- On saute les espaces
+        procedure NextV is
+        begin
+            while CarCour /= 'v' loop
+                AvCar;
+            end loop;
+        end;
+
+    begin
+        NextV;
+        -- On arrive sur un v, il faut voir si on a "vertex"
+        if Lire6 = "vertex" then
+            -- On se trouve sur le 'x' de vertex, on va avancer d'un caractère puis lire 3 float
+            AvCar;
+            for I in 1..3 loop
+                Buffer := U.Null_Unbounded_String;
+                AvCar;
+                while CarCour /= ' ' loop
+                    U.Append(Source => Buffer, New_Item => CarCour);
+                end loop;
+                    Facette(I) := Float'Value(U.To_String(Buffer));
+                end loop;
+        else
+            NextV;
+        end if;
+
+        return Facette;
+    end;
+
     function Chargement_ASCII(Nom_Fichier : String) return Maillage is
         Nb_Facettes : Natural;
         M : Maillage;
         F : File_Type;
+        Facette : Vecteur(1..3);
+        Pos : Positive := 1;
     begin
         Nb_Facettes := Nombre_Facettes(Nom_Fichier);
         -- une fois qu'on a le nombre de facettes on connait la taille du maillage
@@ -57,7 +116,15 @@ package body STL is
         -- on ouvre de nouveau le fichier pour parcourir les facettes
         -- et remplir le maillage
         Open(File => F, Mode => In_File, Name => Nom_Fichier);
-        -- a faire...
+        while not End_of_File(F) loop
+            declare
+                Ligne : String := Get_Line(F);
+            begin
+                Facette := Parse_Ligne(Ligne);
+                M(Pos) := Facette;
+                Pos := Pos + 1;
+            end;
+        end loop;
         Close (F);
         return M;
     end;
