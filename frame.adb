e52aa0920fc65679bfdd9d4f4ff309a2c7848ca6
Mehdi Bahri <mehdi.bahri@ensimag.grenoble-inp.fr>
2013-10-25 02:58:10 +0200

    problème de tracé partiel non fonctionnel à gauche et en haut

--- frame.adb
+++ frame.adb
@@ -1,3 +1,4 @@
+with Ada.Text_IO; use Ada.Text_IO;
 with Dessin; use Dessin;
 with Ligne; use Ligne;
 with STL ; use STL;
@@ -8,7 +9,7 @@ with Scene; use Scene;
 package body Frame is
 
     procedure Tracer_Segment_Secu(Xf1, Yf1, Xf2, Yf2 : in Float) is
-        a, b : Float := 0.0;
+        a, b, c : Float := 0.0;
         X1, X2, Y1, Y2 : Integer := 0;
         Xp1, Xp2, Yp1, Yp2 : Integer := -1;
     begin
@@ -20,54 +21,82 @@ package body Frame is
         end if;
             
         if (X1 <= SCRW) and then (Y1 <= SCRH) and then (X2 <= SCRW) and then (Y2 <= SCRH)  then
-            Tracer_Segment(X1,Y1,X2,Y2);
+            Tracer_Segment(X1,Y1,X2,Y2);    
+        elsif X1 /= X2 and then Y1 /= Y2 then
+            a := Float(Y2 - Y1);
+            b := Float(X1 - X2);
+            c := -(b*Float(Y1) + a*Float(X1));
+
+            if X1 <= 100 then
+                Put_Line("Enfin ! X1");
+                X1 := 100;
+                Y1 := Integer(-(c + a*100.0)/b);
+            elsif X1 > SCRW then
+                X1 := SCRW;
+                Y1 := Integer(-(c + a*Float(SCRW))/b);
+            end if;
+
+            if X2 <= 100 then
+                Put_Line("Enfin ! X2");
+                X2 := 100;
+                Y2 := Integer(-(c + a*100.0)/b);
+            elsif X2 > SCRW then
+                X2 := SCRW;
+                Y2 := Integer(-(c + a*Float(SCRW))/b);
+            end if;
+
+            if Y1 <= 100 then
+                Put_Line("Enfin ! Y1");
+                Y1 := 100;
+                X1 := Integer(-(c+b*100.0)/a);
+            elsif Y1 > SCRH then
+                Y1 := SCRH;
+                X1 := Integer(-(c+b*Float(SCRH))/a);
+            end if;
+
+            if Y2 <= 100 then
+                Put_Line("Enfin ! Y2");
+                Y2 := 100;
+                X2 := Integer(-(c+b*100.0)/a);
+            elsif Y2 > SCRH then
+                Y2 := SCRH;
+                X2 := Integer(-(c+b*Float(SCRH))/a);
+            end if;
+            if X1 in 1..SCRW and then X2 in 1..SCRW and then Y1 in 1..SCRH and then Y2 in 1..SCRH then
+                Tracer_Segment(X1,Y1,X2,Y2);
+            end if;
        -- else
-           -- if not (Y2 = Y1) and then (not (X2 = X1)) then
-           --     a := Float(Y2-Y1)/Float(X2-X1);
-           -- else
-           --     a := 1.0;
-           -- end if;
+       --     if X1 = X2 then
+       --         if X1 > SCRW then
+       --             X1 := SCRW;
+       --             Y1 := Integer(-(c + a*Float(SCRW))/b);
+       --             X2 := SCRW;
+       --             Y2 := Integer(-(c + a*Float(SCRW))/b);
+       --         elsif X1 <= 100 then
+       --             X1 := 100;
+       --             Y1 := Integer(-(c + a*100.0)/b);
+       --             X2 := 100;
+       --             Y2 := Integer(-(c + a*100.0)/b);
+       --         end if;
+       --     end if;
 
-           -- b := Float(Y1) - a*Float(X1);
-
-           -- if a /= 0.0 then
-           --     Xp1 := Integer((1.0 - b)/a);
-           --     Xp2 := Integer((Float(SCRH) - b)/a);
-           -- else
-           --     Xp1 := X1; Xp2 := X2;
-           -- end if;
-
-           -- Yp1 := Integer(a+b);
-           -- Yp2 := Integer(a*Float(SCRW) + b);
-
-           -- if X1 <= SCRW and then Y1 <= SCRH then
-           --     if Xp1 in 1..SCRW then
-           --         X2 := Xp1;
-           --     else
-           --         X2 := Xp2;
-           --     end if;
-
-           --     if Yp1 in 1..SCRW then
-           --         Y2 := Yp1;
-           --     else
-           --         Y2 := Yp2;
-           --     end if;
-           -- end if; 
-
-           -- if X2 <= SCRW and then Y2 <= SCRH then
-           --     if Xp1 in 1..SCRW then
-           --         X1 := Xp1;
-           --     else
-           --         X1 := Xp2;
-           --     end if;
-
-           --     if Yp1 in 1..SCRW then
-           --         Y1 := Yp1;
-           --     else
-           --         Y1 := Yp2;
-           --     end if;
-           -- end if; 
+       --     if Y1 = Y2 then
+       --         if Y1 > SCRH then
+       --             Y1 := SCRH;
+       --             X1 := Integer(-(c+b*Float(SCRH))/a);
+       --             Y2 := SCRH;
+       --             X2 := Integer(-(c+b*Float(SCRH))/a);
+       --         elsif Y1 <= 100 then
+       --             Y1 := 100;
+       --             X1 := Integer(-(c+b*100.0)/a);
+       --             Y2 := 100;
+       --             X2 := Integer(-(c+b*100.0)/a);
+       --         end if;
+       --     end if;
+       --     Tracer_Segment(X1,Y1,X2,Y2);    
         end if;
+    --exception
+        --when CONSTRAINT_ERROR => null;
     end;
 
     procedure Calcul_Image is
@@ -82,6 +111,7 @@ package body Frame is
             end loop;
         end loop;
         -- a faire : calcul des projections, affichage des triangles
+        -- Tracer_Segment_Secu(1.0,1.0,Float(SCRW),Float(SCRH));
         Nb_Facette:=Nombre_De_Facettes;
         for i in 1..Nb_Facette loop
             Projection_Facette(i, P1, P2, P3);
@@ -90,8 +120,5 @@ package body Frame is
             Tracer_Segment_Secu(P1(1), P1(2), P2(1), P2(2));
             Tracer_Segment_Secu(P2(1), P2(2), P3(1), P3(2));
         end loop;
---    exception
---        when CONSTRAINT_ERROR =>
---            null;
     end ;
 end Frame;
